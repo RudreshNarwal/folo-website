ee0257f0b55f0deadd25b8269d043a19e36be51f
Rudresh <rudresh.narwal@gmail.com>
2025-05-13 15:36:59 +0530

    Update Next.js configuration to enable static site generation by adding output: "export". Also, standardize quotes in the remote image patterns for consistency.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // By adding output: 'export', the next build command will generate a fully static version of your site in the out directory. This out directory is what you've (presumably) told Firebase to use as your public directory.
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -11,10 +12,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
